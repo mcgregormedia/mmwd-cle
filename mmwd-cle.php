72e2cd62ce1d9a9257a8cd4d65c3ab06f22450ef
Andy McGregor <info@mcgregormedia.co.uk>
2020-08-08 10:17:37 +0100

    v1.8.0
    1.8.0 08-08-2020
    UPDATED: Tested up to WordPress 5.5

--- mmwd-cle.php
+++ mmwd-cle.php
@@ -3,11 +3,9 @@
 Plugin Name: MMWD Custom Login Error
 Plugin URI: https://mcgregormedia.co.uk/projects/mmwd-custom-login-error/
 Description: Hide whether it's the username or password that is wrong on a failed login.
-Version: 1.7.0
+Version: 1.8.0
 Author: McGregor Media Web Design
 Author URI: https://mcgregormedia.co.uk
-Requires at least: 4.2
-Tested up to: 5.4
 License: GNU General Public License v3.0
 License URI: http://www.gnu.org/licenses/gpl-3.0.html
 */
